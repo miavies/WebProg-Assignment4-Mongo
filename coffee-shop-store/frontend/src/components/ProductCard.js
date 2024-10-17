f1efe9ba10dbee9b40cd67f9954938079aabe074
CIITSHSDTP00\CIIT <michaelasantiago25@gmail.com>
2024-10-17 13:37:56 +0800

    commit frontend

---
+++ coffee-shop-store/frontend/src/components/ProductCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// import './ProductCard.css';
+
+const ProductCard = ({ product, addToCart }) => {
+
+  return (
+
+    <div className="product-card">
+
+      <img src={product.imageUrl} alt={product.name} />
+
+      <h3>{product.name}</h3>
+
+      <p>₱{product.price}</p>
+      <p>{product.description}</p>
+
+      <button onClick={() => addToCart(product)}>Add to Cart</button>
+
+    </div>
+
+  );
+
+};
+
+export default ProductCard;
